88bdf1c2a27279a06bad1e8a6b24de58c7cca2db
Joel Gnansounou <freshdjr@gmail.com>
2024-01-15 11:23:09 +0100

    feat: make the contract burnable

--- contracts/FreshNetToken.sol
+++ contracts/FreshNetToken.sol
@@ -4,8 +4,9 @@ pragma solidity ^0.8.9;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract FreshNetToken is ERC20Capped {
+contract FreshNetToken is ERC20Capped, ERC20Burnable {
     uint256 constant max_supply = 50000000;
     uint256 timeDecimals = 10 ** decimals();
 
