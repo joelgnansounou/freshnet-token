1c352970bc2df9ab32961aeb0a595529fa830fb8
Joel Gnansounou <freshdjr@gmail.com>
2024-01-15 06:25:59 +0100

    feat: set the initial and max supply for FNT token

--- contracts/FreshNetToken.sol
+++ contracts/FreshNetToken.sol
@@ -2,6 +2,17 @@
 
 pragma solidity ^0.8.9;
 
-contract FreshNetToken {
-    
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+
+contract FreshNetToken is ERC20Capped {
+    uint256 constant max_supply = 50000000;
+    uint256 timeDecimals = 10 ** decimals();
+
+    constructor()
+        ERC20("FreshNet Token", "FNT")
+        ERC20Capped(max_supply * timeDecimals)
+    {
+        _mint(msg.sender, ((max_supply * 50) / 100) * timeDecimals);
+    }
 }
