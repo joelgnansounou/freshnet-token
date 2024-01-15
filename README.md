272ac2e0bceb0b3a73535a63708fdb7ab01b43e2
Joel Gnansounou <freshdjr@gmail.com>
2024-01-15 05:44:49 +0100

    build: init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# FreshNet Token
+
---
+++ scripts/deploy.js
@@ -0,0 +1,9 @@
+const { ethers } = require("hardhat");
+
+(async () => {
+  try {
+  } catch (error) {
+    console.error(error);
+    process.exitCode = 1;
+  }
+})();
